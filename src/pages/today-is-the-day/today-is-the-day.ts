c15cca6eef995efe825060bb4cffda0d1e4abeda
chris-caidson <chris@caidson.com>
2017-12-28 17:51:38 -0500

    Implemented Today is the Day page.

--- src/pages/today-is-the-day/today-is-the-day.ts
+++ src/pages/today-is-the-day/today-is-the-day.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, ModalController } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -7,5 +7,38 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "today-is-the-day.html"
 })
 export class TodayIsTheDayPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
+
+  openVideoModal(video: string) {
+    var data: any;
+
+    switch (video) {
+      case "today":
+        data = {
+          name: "Today is the Day",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FToday%20is%20the%20Day.mp4?alt=media&token=8ad99737-c6ae-4245-a191-ff7a681f4fb7"
+        };
+        break;
+
+      case "freedom":
+        data = {
+          name: "Freedom Plan",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2Ffinal-freedom-video.mp4?alt=media&token=82af18c3-30cd-440d-9a01-af90bf3e7b77"
+        };
+        break;
+
+      case "vision":
+        data = {
+          name: "The TIME is NOW!",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FGMT20171102-010719_Herb-Patty_2560x1440.mp4?alt=media&token=f825ce87-c083-4801-8f77-2419aecb58f7"
+        };
+        break;
+    }
+
+    const myModal = this.modalCtrl.create("VideoModalPage", data);
+    myModal.present();
+  }
 }
