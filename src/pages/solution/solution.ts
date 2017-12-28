bed12afbcbf9d722ab8b6f5ed71199e11fe070b4
chris-caidson <chris@caidson.com>
2017-12-28 16:14:40 -0500

    Implemented Solution page.

--- src/pages/solution/solution.ts
+++ src/pages/solution/solution.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, ModalController } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -7,5 +7,38 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "solution.html"
 })
 export class SolutionPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, private modalCtrl: ModalController) {}
+
+  openVideoModal(video: string) {
+    var data: any;
+
+    switch (video) {
+      case "toxic":
+        data = {
+          name: "Are You Toxic?",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FAre-You-Toxic_-Isagenix-Promo-Length-YouTube-360p.mp4?alt=media&token=786d30b2-ebb6-41b6-82f3-fc5a68645c94"
+        };
+        break;
+
+      case "cleansing":
+        data = {
+          name: "Nutritional Cleansing",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FMary-Evans-on-the-power-of-the-Isagenix-System---YouTube-(480p).mp4?alt=media&token=8cb0b61a-7d39-4871-a5f2-c50e2f007b2f"
+        };
+        break;
+
+      case "isa-works":
+        data = {
+          name: "Why Isagenix Works",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FMichael.mp4?alt=media&token=cb23a367-b6b9-48e5-8a9a-d4ee9db8c4f4"
+        };
+        break;
+    }
+
+    const myModal = this.modalCtrl.create("VideoModalPage", data);
+    myModal.present();
+  }
 }
