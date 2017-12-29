be8faebebdde60524b329f486bdd45f84fe1ffed
chris-caidson <chris@caidson.com>
2017-12-29 12:23:58 -0500

    Implemented Eat It, Love It, Share It page and changed styling of navbar.

--- src/pages/eat-it-love-it-share-it/eat-it-love-it-share-it.ts
+++ src/pages/eat-it-love-it-share-it/eat-it-love-it-share-it.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, ModalController } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -7,5 +7,14 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "eat-it-love-it-share-it.html"
 })
 export class EatItLoveItShareItPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
+
+  openVideoModal() {
+    const myModal = this.modalCtrl.create("VideoModalPage", {
+      name: "You Share, They Share, Repeat",
+      url: "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FYou%20Share%2C%20They%20Share%2C%20Repeat.mp4?alt=media&token=8324d58b-a37e-4e04-9ab7-e04774330518"
+    });
+
+    myModal.present();
+  }
 }
