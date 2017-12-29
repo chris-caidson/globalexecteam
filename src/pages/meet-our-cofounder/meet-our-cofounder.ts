822274e2fea8f7581b1d8c02c749a01ebb80a67a
chris-caidson <chris@caidson.com>
2017-12-29 10:52:28 -0500

    Implemented Meet Our Cofounder page.

--- src/pages/meet-our-cofounder/meet-our-cofounder.ts
+++ src/pages/meet-our-cofounder/meet-our-cofounder.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, ModalController } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -7,5 +7,14 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "meet-our-cofounder.html"
 })
 export class MeetOurCofounderPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
+
+  openVideoModal() {
+    const myModal = this.modalCtrl.create("VideoModalPage", {
+      name: "Meet Our Cofounder - Kathy Coover",
+      url: "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2FSusan%20Miller%20with%20Kathy%20Coover%20-%20How%20Isagenix%20Got%20it%20Right!.mp4?alt=media&token=18cafa7c-4c84-48a9-9440-22498363739e"
+    });
+
+    myModal.present();
+  }
 }
