6bb8cf1f85a6d028ca5cab9a31934ba8be7a7731
chris-caidson <chris@caidson.com>
2018-01-07 12:09:02 -0500

    Implemented Our Mission page.

--- src/pages/get-our-mission/get-our-mission.ts
+++ src/pages/get-our-mission/get-our-mission.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, ModalController } from "ionic-angular";
 
 @IonicPage()
 @Component({
@@ -7,5 +7,30 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "get-our-mission.html"
 })
 export class GetOurMissionPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {}
+
+  openVideoModal(video: string) {
+    var data: any;
+
+    switch (video) {
+      case "get-party":
+        data = {
+          name: "GET Party",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2Fget-party.mp4?alt=media&token=09fb9ffb-d437-4d9a-ba42-d88cbb215f5d"
+        };
+        break;
+
+      case "our-mission":
+        data = {
+          name: "What's Your 'Why'?",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2Fi-am-good-enough.mp4?alt=media&token=cca97046-24dd-40aa-9be3-bd8101441ee1"
+        };
+        break;
+    }
+
+    const myModal = this.modalCtrl.create("VideoModalPage", data);
+    myModal.present();
+  }
 }
