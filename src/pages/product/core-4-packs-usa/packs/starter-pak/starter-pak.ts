e082ff587a57b0915c9d9621ea11a94f23612e35
chris-caidson <chris@caidson.com>
2018-01-11 18:51:56 -0500

    Finished implementing Pack pages.

--- src/pages/product/core-4-packs-usa/packs/starter-pak/starter-pak.ts
+++ src/pages/product/core-4-packs-usa/packs/starter-pak/starter-pak.ts
@@ -16,19 +16,27 @@ export class StarterPakPage {
     var data: any;
 
     switch (video) {
-      case "":
+      case "shake-day":
         data = {
-          name: "",
+          name: "How to do an Isagenix Shake Day",
           url:
-            ""
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2Fhow-to-do-a-shake-day.mp4?alt=media&token=2015e97e-bb87-4a0c-85ea-9e664ca13a7f"
         };
         break;
 
-      case "":
+      case "cleanse-day":
         data = {
-          name: "",
+          name: "How to do an Isagenix Cleanse Day",
           url:
-            ""
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2Fhow-to-do-a-cleanse-day.mp4?alt=media&token=7e02e057-d60e-47ca-b6cb-763c61ba9e87"
+        };
+        break;
+
+        case "30-day":
+        data = {
+          name: "How to Use the 30-Day System",
+          url:
+            "https://firebasestorage.googleapis.com/v0/b/globalexecteam-2018.appspot.com/o/videos%2Fhow-to-use-the-30-day-system.mp4?alt=media&token=ea977e8c-042a-40b8-ac6b-924a6af7152e"
         };
         break;
     }
