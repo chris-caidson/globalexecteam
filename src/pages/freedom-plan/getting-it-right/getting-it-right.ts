44615e96a99d8bcb1d8b5a3765b2bb54d2f0ca27
chris-caidson <chris@caidson.com>
2018-11-20 05:55:34 -0500

    Updated Getting it Right page.

--- src/pages/freedom-plan/getting-it-right/getting-it-right.ts
+++ src/pages/freedom-plan/getting-it-right/getting-it-right.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { IonicPage, NavController, NavParams, ModalController } from "ionic-angular";
 import { PdfProvider } from "../../../providers/pdf/pdf";
 import { GoogleAnalyticsProvider } from './../../../providers/google-analytics/google-analytics';
 import { AuthProvider } from './../../../providers/auth/auth';
@@ -15,10 +15,21 @@ export class GettingItRightPage {
     public navParams: NavParams,
     public pdfProvider: PdfProvider,
     private gap: GoogleAnalyticsProvider,
-    public auth: AuthProvider
+    public auth: AuthProvider,
+    public modalCtrl: ModalController
   ) { }
 
   ionViewWillLoad() {
     this.gap.trackView("GettingItRightPage");
+  }
+
+  openVideoModal(video: string) {
+    const myModal = this.modalCtrl.create("VideoModalPage", {
+      name: "The Coming of Age of an Industry",
+      url:
+        "https://firebasestorage.googleapis.com/v0/b/getfreeglobal.appspot.com/o/videos%2Fcoming-of-age.mp4?alt=media&token=151ee0ee-951a-4e6f-a2a0-28e74bd61766"
+    });
+
+    myModal.present();
   }
 }
